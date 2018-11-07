bad1fcf15b72935739146961a6b1e87f1ff2214d
stoltzmaniac <scott@stoltzmanconsulting.com>
2018-11-07 16:57:10 -0700

    Clean up database access

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from config import Config
 from my_app.models import Base, User
 
 
-db_name = 'postgres://quart:quart@localhost:5432/quart'
+db_name = Config.DATABASE_URI
 
 engine = create_engine(db_name)
 session = sessionmaker()
