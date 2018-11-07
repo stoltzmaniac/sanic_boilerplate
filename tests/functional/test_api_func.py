a5a56ece72b955e09f2e1d463a6c31ee0e92ff94
stoltzmaniac <scott@stoltzmanconsulting.com>
2018-11-07 08:40:52 -0700

    Add basic test

--- tests/functional/test_api_func.py
+++ tests/functional/test_api_func.py
@@ -1,6 +1,8 @@
-from run import app
-
-
-def test_index_returns_200():
-    request, response = app.test_client.get('/')
-    assert response.status == 200
+async def test_index_page(test_cli):
+    """
+    GIVEN a Sanic application
+    WHEN the '/' page is requested (GET)
+    THEN check the response is valid
+    """
+    resp = await test_cli.get('/')
+    assert resp.status == 200
