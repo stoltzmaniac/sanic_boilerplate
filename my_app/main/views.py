4c8fa4e01bf83c5504980a5341fa100b00e8e0a9
stoltzmaniac <scott@stoltzmanconsulting.com>
2018-11-08 14:25:29 -0700

    Add all templates from bootstrap theme

--- my_app/main/views.py
+++ my_app/main/views.py
@@ -2,7 +2,7 @@ from sanic import Blueprint
 from sanic.response import json, text, html
 from jinja2 import Environment, PackageLoader
 
-env = Environment(loader=PackageLoader('my_app', 'static'))
+env = Environment(loader=PackageLoader('my_app', 'templates'))
 
 main = Blueprint('main', url_prefix='/')
 
