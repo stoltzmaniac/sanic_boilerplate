fb8ba5156f45cc015d0e10e99f5db5a786f89e6a
stoltzmaniac <scott@stoltzmanconsulting.com>
2018-11-08 13:47:10 -0700

    Add templates with import

--- my_app/users/views.py
+++ my_app/users/views.py
@@ -1,12 +1,16 @@
 from sanic import Blueprint
-from sanic.response import json, text
+from sanic.response import json, text, html
+from jinja2 import Environment, PackageLoader
 
+env = Environment(loader=PackageLoader('my_app', 'templates'))
 
 users = Blueprint('users', url_prefix='/users')
 
 @users.route('/')
 async def index(request):
-    return json({'users_index': 'page'})
+    template = env.get_template('users/index.html')
+    content = template.render(some_text='YAY - this is working!')
+    return html(content)
 
 @users.route('/json')
 async def index_json(request):
